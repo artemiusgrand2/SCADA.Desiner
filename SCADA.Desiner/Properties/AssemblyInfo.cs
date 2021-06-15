33c40da62322027d6deb544bc98ed67ab19e2a94
dubina <artemiusgrand@gmail.com>
2021-06-15 13:56:45 +0300

    update aligment menu in ToolsGraphics

--- SCADA.Desiner/Properties/AssemblyInfo.cs
+++ SCADA.Desiner/Properties/AssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Windows;
 [assembly: AssemblyTitle("SCADA.Desiner")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("MICROSOFT")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SCADA.Desiner")]
-[assembly: AssemblyCopyright("Copyright © MICROSOFT 2015")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
